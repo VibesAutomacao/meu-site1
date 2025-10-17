600505eddfc04030051650680be437b76c86cc52
JulioCesar <juliojbs007@gmail.com>
2025-10-17 19:40:36 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,124 @@
+//String "Ola Java"
+//Numero "1,2,3"
+//Bolean "True, False
+
+
+//var nome = "Pedro"
+//var idade = 15
+//var aprovado = true
+//console.log(typeof nome)
+//console.log(typeof idade)
+//console.log(typeof aprovado)
+
+//Sequencia Ctrl+shift+Baixo
+//console.log("Ola Mundo 1");
+//console.log("Ola Mundo 2");
+//console.log("Ola Mundo 3");
+//console.log("Ola Mundo 4");
+
+
+//var nome = "Ola JavaScript"
+
+//console.log(nome);
+//console.log(nome);
+
+
+//TRECHO RESPONSAVEL PELO PEDIDO
+var nome = "Pedro"
+var idade = 23
+
+
+console.log("Ola Seja Bem Vindo A Sorveteria");
+console.log("Seu Pedido Eo Seguinte...");
+console.log("-------------------------")
+
+var sabor = "Chocolate"
+var QuantidadeBolas = 2
+var PrecoPBola = 2.5
+var ValorTotal = QuantidadeBolas * PrecoPBola
+
+console.log("Sabor é",sabor);
+console.log("Quantidade de Bolas",QuantidadeBolas);
+console.log("Valor Total",ValorTotal );
+
+
+
+
+
+//Operadores Aritimeticos
+// + adição
+console.log("Adição", 5 + 5);
+
+// - subtração
+console.log("subtração", 5 - 5);
+
+// / Divisão
+console.log("Divisão", 5 / 5);
+
+// * Multripicaçao
+console.log("Multiplicação", 5 * 5);
+
+// ** Exponencial
+console.log("Exponencial", 5 ** 5);
+
+// % Resto De Divisão
+console.log("Resto De Divisao", 5 % 3);
+
+//Aritimetico Atribuiçao
+var numero = 1
+numero--
+console.log(numero);
+numero = numero + 1 * 2
+console.log(numero);
+
+//Operadores de Atribuiçao
+var numero = 10
+numero += 10
+console.log(numero);
+
+
+// console.log(10 / 5 + 3 * 5); TRNAFORMAR SEQUENCIA CERTA DA MATEMATICA
+
+// EXPRESSOES BOLEANO
+var nota = 7
+
+// OPERADORES DE COMPARAÇAO > maior q , < menor q , == Igual q , === Estritamente igual , ! diferente de
+
+var aprovado = (nota > 5)
+console.log(aprovado)
+
+var reprovado = (nota < 8)
+console.log(reprovado);
+
+var recuperaçao = (nota == 7)
+console.log(recuperaçao);
+
+// Operadores Logicos
+// && AND ---> Condiçao 1 Condiçao 2 TRUE -> true
+// "dois pau reto " OR ---> Cond 1 Cond 2 TRUE -> true
+var nota = 5
+var BomAluno = true
+
+var aprovado = (nota >= 5 && BomAluno == true)
+
+//If Else (se , se nao)
+var nota = 2
+
+// se (nota > 5) {Resposta}
+if (nota >= 5) {
+    console.log("Aluno Aprovado");
+    
+} else if (nota == 5) {
+    console.log("Passou Raspando");
+    
+}else (nota < 5);{
+    console.log("Aluno Reprovado");
+    
+}
+
+
+
+
+
+
+
